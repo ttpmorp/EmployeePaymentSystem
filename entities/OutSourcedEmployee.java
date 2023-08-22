db624145aba71afe29a5ccd4531f8cd3ba9aad9b
juLia <84445011+ttpmorp@users.noreply.github.com>
2023-08-22 14:00:09 -0300

    Class
    classes do programa

---
+++ entities/OutSourcedEmployee.java
@@ -0,0 +1,30 @@
+package entities;
+
+public class OutSourcedEmployee extends Employee{
+	private Double additionalCharge;
+
+	
+	public OutSourcedEmployee() {
+		super();
+	}
+	
+	public Double getAdditionalCharge() {
+		return additionalCharge;
+	}
+
+	public void setAdditionalCharge(Double additionalCharge) {
+		this.additionalCharge = additionalCharge;
+	}
+
+	public OutSourcedEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+		super(name, hours, valuePerHour);
+		this.additionalCharge = additionalCharge;
+	}
+	
+	@Override
+	public double payment() {
+		return super.payment() + additionalCharge * 1.1;
+	}
+	
+
+}
